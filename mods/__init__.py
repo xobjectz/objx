0cea249521dfd9a6a6b5d996bf7da1c1cdb4f706
Bart Thate <objx@proton.me>
2024-02-01 18:07:34 +0100

    fix where

--- mods/__init__.py
+++ mods/__init__.py
@@ -9,13 +9,7 @@
 import sys
 
 
-def getmain(name):
-    return getattr(sys.modules["__main__"], name, None)
-
-
-
-from . import cmd, err, fnd, irc, log, mod, mre, pwd, rss, tdo, thr, tmr
-from . import rst, udp
+from . import cmd, err, fnd, irc, log, mod, rss, rst, tdo, thr, tmr, udp
 
 
 
@@ -24,7 +18,6 @@ def __dir__():
         'cmd',
         'err',
         'fnd',
-        'getmain',
         'irc',
         'log',
         'mod',
