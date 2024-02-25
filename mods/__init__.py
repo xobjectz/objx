710cca957b45195166f2ac9ac330c7380aecbfa7
Bart Thate <bthate@dds.nl>
2024-02-25 05:45:55 +0100

    add mods

---
+++ mods/__init__.py
@@ -0,0 +1,36 @@
+# This file is placed in the Public Domain.
+#
+# ruff: noqa: F401
+
+
+"modules"
+
+
+from . import irc, log, mod, mre, pwd, rss, tdo, flt, thr
+from . import mdl, req, slg
+
+
+def __geno__():
+    return (
+        'mdl',
+        'req',
+        'slg'
+    )
+
+
+def __dir__():
+    return (
+        'flt',
+        'irc',
+        'log',
+        'mod',
+        'mre',
+        'pwd',
+        'rss',
+        'shl',
+        'tdo',
+        'thr'
+    ) + __geno__()
+
+
+__all__ = __dir__()
