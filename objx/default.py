4e6b46dcae651ac954de368cc4ec54d1d646d7d3
Bart Thate <objx@proton.me>
2024-03-05 22:18:07 +0100

    add default

---
+++ objx/default.py
@@ -0,0 +1,24 @@
+# This file is placed in the Public Domain,
+#
+# pylint: disable=C,R,W0105,W0613,E0101
+
+
+"objects"
+
+
+import json
+
+
+from .objects import Object
+
+
+class Default(Object):
+
+    __slots__ = ("__default__",)
+
+    def __init__(self):
+        Object.__init__(self)
+        self.__default__ = ""
+
+    def __getattr__(self, key):
+        return self.__dict__.get(key, self.__default__)
