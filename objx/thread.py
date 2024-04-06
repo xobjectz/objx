a5eb9737fc19f719ee5701e9bbab3c4d07e73ea2
Bart Thate <bthate@dds.nl>
2024-04-06 14:49:42 +0200

    move to bin/objx

--- objx/thread.py
+++ objx/thread.py
@@ -14,6 +14,9 @@ import traceback
 import types
 
 
+from .errors import Errors
+
+
 class Thread(threading.Thread):
 
     "Thread"
@@ -50,70 +53,6 @@ class Thread(threading.Thread):
                 args[0].ready()
 
 
-class Errors:
-
-    "Errors"
-
-    errors = []
-    filter = []
-    output = None
-    shown  = []
-
-    @staticmethod
-    def add(exc):
-        "add an exception"
-        excp = exc.with_traceback(exc.__traceback__)
-        Errors.errors.append(excp)
-
-    @staticmethod
-    def enable(out):
-        "enable output"
-        Errors.output = out
-
-    @staticmethod
-    def format(exc):
-        "format an exception"
-        res = ""
-        stream = io.StringIO(
-                             traceback.print_exception(
-                                                       type(exc),
-                                                       exc,
-                                                       exc.__traceback__
-                                                      )
-                            )
-        for line in stream.readlines():
-            res += line + "\n"
-        return res
-
-    @staticmethod
-    def out(exc):
-        "check if output function is set."
-        if Errors.output is None:
-            return
-        txt = str(Errors.format(exc))
-        Errors.output(txt)
-
-    @staticmethod
-    def show():
-        "show exceptions"
-        for exc in Errors.errors:
-            Errors.out(exc)
-
-    @staticmethod
-    def skip(txt):
-        "check for skipping exceptions"
-        for skp in Errors.filter:
-            if skp in str(txt):
-                return True
-        return False
-
-
-def debug(txt):
-    "debug text"
-    if Errors.output and not Errors.skip(txt):
-        Errors.output(txt)
-
-
 def launch(func, *args, **kwargs):
     "launch a thread."
     nme = kwargs.get("name", name(func))
@@ -143,9 +82,7 @@ def name(obj):
 
 def __dir__():
     return (
-        'Errors',
         'Thread',
-        'debug',
         'launch',
         'name'
     )
