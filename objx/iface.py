6e365e1000dbc5a8148f32aec4be88b4348bff98
Bart Thate <bthate@dds.nl>
2024-06-13 16:53:08 +0200

    update iface

--- objx/iface.py
+++ objx/iface.py
@@ -1,12 +1,13 @@
 # This file is placed in the Public Domain.
 #
-# pylint: disable=C,R,W0105
+# pylint: disable=W0401,W0614
 
 
 "objects"
 
 
 from .object  import *
+from .object  import __dir__ as __odir__
 from .decoder import read
 from .default import Default
 from .encoder import write
@@ -17,21 +18,10 @@ def __dir__():
     return (
         'Config',
         'Default',
-        'Object',
-        'construct',
-        'edit',
-        'fmt',
-        'fqn',
-        'ident',
-        'items',
-        'keys',
-        'match',
         'read',
-        'search',
-        'update',
-        'values',
         'write'
-    )
+    ) + __odir__()
 
 
 __all__ = __dir__()
+
