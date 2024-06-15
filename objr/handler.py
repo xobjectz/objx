f34747597d28e83a8be08e7cf16c60dcf10e0736
Bart Thate <bthate@dds.nl>
2024-06-15 08:17:39 +0200

    fix tinder

--- objr/handler.py
+++ objr/handler.py
@@ -128,6 +128,45 @@ class Handler:
         self.stopped.set()
 
 
+class CLI(Handler):
+
+    "CLI"
+
+    out = None
+
+
+    def __init__(self):
+        Handler.__init__(self)
+        self.register("command", command)
+
+    def say(self, _channel, txt):
+        "echo on verbose."
+        self.raw(txt)
+
+    def raw(self, txt):
+        "print to screen."
+        if CLI.out:
+            txt = txt.encode('utf-8', 'replace').decode()
+            CLI.out(txt)
+
+    def show(self, evt):
+        "show results into a channel."
+        for txt in evt.result:
+            self.say(evt.channel, txt)
+
+
+def cmnd(txt, outer):
+    "do a command using the provided output function."
+    cli = CLI()
+    cli.out = outer
+    evn = Event()
+    evn.orig = repr(cli)
+    evn.txt = txt
+    command(cli, evn)
+    evn.wait()
+    return evn
+
+
 def command(bot, evt):
     "check for and run a command."
     parse(evt)
@@ -143,6 +182,7 @@ def __dir__():
         'Commands',
         'Event',
         'Handler',
-        'command'
+        'cmnd',
+        'command',
         'scan'
     )
