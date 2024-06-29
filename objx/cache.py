64bf0e08dfbe1345395b2f1d2502efd021aadc57
Bart thate <bthate@dds.nl>
2024-06-29 22:14:45 +0200

    nit

--- objx/cache.py
+++ objx/cache.py
@@ -1,7 +1,7 @@
 # This file is placed in the Public Domain.
 
 
-"broker"
+"caching"
 
 
 from .lock   import brokerlock
--- objx/cmds.py
+++ objx/cmds.py
@@ -4,8 +4,32 @@
 "commands"
 
 
-from .object import Object
+from .encode import dumps
+from .object import Object, construct
 from .parse  import parse
+from .table  import ondemand
+
+
+MODNAMES = {
+    "cmd": "objx.mods.cmd",
+    "dmp": "objx.mods.dmp",
+    "err": "objx.mods.err",
+    "fnd": "objx.mods.fnd",
+    "cfg": "objx.mods.irc",
+    "mre": "objx.mods.irc",
+    "pwd": "objx.mods.irc",
+    "dpl": "objx.mods.rss",
+    "nme": "objx.mods.rss",
+    "rem": "objx.mods.rss",
+    "res": "objx.mods.rss",
+    "rss": "objx.mods.rss",
+    "syn": "objx.mods.rss",
+    "exp": "objx.mods.rss",
+    "imp": "objx.mods.rss",
+    "thr": "objx.mods.thr",
+    "tmr": "objx.mods.tmr",
+    "upt": "objx.mods.upt"
+}
 
 
 class Commands:
@@ -14,27 +38,40 @@ class Commands:
 
     cmds     = Object()
     modnames = Object()
+    construct(modnames, MODNAMES)
 
     
 def add(func):
     "add command."
     setattr(Commands.cmds, func.__name__, func)
-    setattr(Commands.modnames, func.__name__, func.__module__)
+    if func.__module__ != "__main__":
+        setattr(Commands.modnames, func.__name__, func.__module__)
 
 
 def command(bot, evt):
     "check for and run a command."
     parse(evt)
     func = getattr(Commands.cmds, evt.cmd, None)
+    if not func:
+        mname = getattr(Commands.modnames, evt.cmd, None)
+        if mname:
+            mod = ondemand(mname)
+            getattr(mod, evt.cmd, None)
+        func = getattr(Commands.cmds, evt.cmd, None)
     if func:
         func(evt)
     bot.show(evt)
     evt.ready()
 
 
+def dump():
+    print(dumps(Commands.modnames, indent=4))
+
+
 def __dir__():
     return (
         'Commands',
         'add',
-        'command'
+        'command',
+        'dump'
     )
